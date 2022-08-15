0bc49551d4abf6e8c0512e6143abb693d9452524
Igor Nikolic <gogicanikolic@gmail.com>
2022-08-15 20:40:21 +0200

    server

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,123 +3,109 @@ using System.Threading;
 using System.Net.Sockets;
 using System.Text;
 using System.Collections;
+using System.Net;
 
 namespace ISP_ChatServer
 {
     class Program
     {
-        public static Hashtable clientsList = new Hashtable();
-
+        public static Hashtable lista_klijenata = new Hashtable();
         static void Main(string[] args)
         {
-            TcpListener serverSocket = new TcpListener(8888);
-            TcpClient clientSocket = default(TcpClient);
-            int counter = 0;
-
-            serverSocket.Start();
+            var localhost = IPAddress.Parse("127.0.0.1");
+            TcpListener socket_servera = new TcpListener(localhost, 8888);
+            TcpClient socket_klijenta = default(TcpClient);
+            int i = 0;
+            socket_servera.Start();
             Console.WriteLine("ISP ChatServer pokrenut.");
-            counter = 0;
             while ((true))
             {
-                counter += 1;
-                clientSocket = serverSocket.AcceptTcpClient();
-
-                byte[] bytesFrom = new byte[10000000];
-                string dataFromClient = null;
-
+                i += 1;
+                socket_klijenta = socket_servera.AcceptTcpClient();
+                byte[] bajtovi = new byte[10000000];
+                string podaci_klijenta = null;
                 try
                 {
-                    NetworkStream networkStream = clientSocket.GetStream();
-                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
-                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
-                    clientsList.Add(dataFromClient, clientSocket);
-                    broadcast(dataFromClient + " je online. ", dataFromClient, false);
-                    Console.WriteLine(dataFromClient + " je online. ");
-                    handleClinet client = new handleClinet();
-                    client.startClient(clientSocket, dataFromClient, clientsList);
+                    NetworkStream stream_mreze = socket_klijenta.GetStream();
+                    stream_mreze.Read(bajtovi, 0, (int)socket_klijenta.ReceiveBufferSize);
+                    podaci_klijenta = System.Text.Encoding.ASCII.GetString(bajtovi);
+                    podaci_klijenta = podaci_klijenta.Substring(0, podaci_klijenta.IndexOf("$"));
+                    lista_klijenata.Add(podaci_klijenta, socket_klijenta);
+                    Objava(podaci_klijenta + " je online. ", podaci_klijenta, false);
+                    Console.WriteLine(podaci_klijenta + " je online. ");
+                    Klijent client = new Klijent();
+                    client.pokreni_klijenta(socket_klijenta, podaci_klijenta, lista_klijenata);
                 }
                 catch (Exception ex)
                 {
                     Console.Write(ex.Message);
                 }
             }
-
-            clientSocket.Close();
-            serverSocket.Stop();
+            socket_klijenta.Close();
+            socket_servera.Stop();
             Console.WriteLine("exit");
             Console.ReadLine();
         }
-
-        public static void broadcast(string msg, string uName, bool flag)
+        public static void Objava(string poruka, string korisnicko_ime, bool oznaka)
         {
-            foreach (DictionaryEntry Item in clientsList)
+            foreach (DictionaryEntry Item in lista_klijenata)
             {
-                TcpClient broadcastSocket;
-                broadcastSocket = (TcpClient)Item.Value;
-                NetworkStream broadcastStream = broadcastSocket.GetStream();
-                Byte[] broadcastBytes = null;
-
-                if (flag == true)
+                TcpClient socket_objave;
+                socket_objave = (TcpClient)Item.Value;
+                NetworkStream stream_objave = socket_objave.GetStream();
+                Byte[] bajtovi = null;
+                if (oznaka == true)
                 {
-                    broadcastBytes = Encoding.ASCII.GetBytes(uName + ": " + msg);
+                    bajtovi = Encoding.ASCII.GetBytes(korisnicko_ime + ": " + poruka);
                 }
                 else
                 {
-                    broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                    bajtovi = Encoding.ASCII.GetBytes(poruka);
                 }
-
-                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
-                broadcastStream.Flush();
+                stream_objave.Write(bajtovi, 0, bajtovi.Length);
+                stream_objave.Flush();
             }
-        }  //end broadcast function
-    }//end Main class
-
-
-    public class handleClinet
-    {
-        TcpClient clientSocket;
-        string clNo;
-        Hashtable clientsList;
-
-        public void startClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
-        {
-            this.clientSocket = inClientSocket;
-            this.clNo = clineNo;
-            this.clientsList = cList;
-            Thread ctThread = new Thread(doChat);
-            ctThread.Start();
         }
-
-        private void doChat()
+    }
+    public class Klijent
+    {
+        TcpClient socket_klijenta;
+        string broj_klijenta;
+        Hashtable lista_klijenata;
+        public void pokreni_klijenta(TcpClient ulazni_socket, string broj, Hashtable lista)
         {
-            int requestCount = 0;
-            byte[] bytesFrom = new byte[1000000];
-            string dataFromClient = null;
-            Byte[] sendBytes = null;
-            string serverResponse = null;
-            string rCount = null;
-            requestCount = 0;
-
-            while ((true))
+            this.socket_klijenta = ulazni_socket;
+            this.broj_klijenta = broj;
+            this.lista_klijenata = lista;
+            Thread thread_klijenta = new Thread(Chat);
+            thread_klijenta.Start();
+        }
+        private void Chat()
+        {
+            int broj_zahteva = 0;
+            byte[] bajtovi_od = new byte[1000000];
+            string podaci_klijenta = null;
+            Byte[] bajtovi_za = null;
+            string odgovor_servera = null;
+            string broj_z_s = null;
+            while (true)
             {
                 try
                 {
-                    requestCount = requestCount + 1;
-                    NetworkStream networkStream = clientSocket.GetStream();
-                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
-                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
-                    Console.WriteLine("Klijent - " + clNo + " : " + dataFromClient);
-                    rCount = Convert.ToString(requestCount);
-
-                    Program.broadcast(dataFromClient, clNo, true);
+                    broj_zahteva += 1;
+                    NetworkStream stream_mreze = socket_klijenta.GetStream();
+                    stream_mreze.Read(bajtovi_od, 0, (int)socket_klijenta.ReceiveBufferSize);
+                    podaci_klijenta = Encoding.ASCII.GetString(bajtovi_od);
+                    podaci_klijenta = podaci_klijenta.Substring(0, podaci_klijenta.IndexOf("$"));
+                    Console.WriteLine("Klijent - " + broj_klijenta + " : " + podaci_klijenta);
+                    broj_z_s = Convert.ToString(broj_zahteva);
+                    Program.Objava(podaci_klijenta, broj_klijenta, true);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.ToString());
                 }
-            }//end while
-        }//end doChat
-    } //end class handleClinet
-}//end namespace
+            }
+        }
+    }
+}
